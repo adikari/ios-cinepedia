ff491d5b2a07315484109bd909f9f3102a396a80
subash adhikari <me@subash.com.au>
2018-05-06 09:56:42 +1000

    Initial Commit

---
+++ ios-cinepedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-cinepedia
+//
+//  Created by subash adhikari on 6/5/18.
+//  Copyright © 2018 subash adhikari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
